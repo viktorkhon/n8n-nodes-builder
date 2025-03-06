bca8687bb7d6885b0e92a308d4179115db4270d7
Mathis Gauthey <mathis.gauthey@gmail.com>
2025-03-06 15:46:22 +0000

    feat: 🛠️ update devcontainer configuration and add custom node linking script
    - Removed unnecessary docker-compose file from the configuration.
    - Added `postStartCommand` to execute a script for linking custom nodes.
    - Introduced `link_custom_nodes.sh` to manage custom node linking in the n8n instance.

---
+++ .devcontainer/link_custom_nodes.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Change the owner of the .n8n directory to the node user
+chown node:node /home/node/.n8n
+
+# Create the /home/node/.n8n/custom directory if it doesn't exist
+mkdir -p /home/node/.n8n/custom
+
+# Go to the custom directory
+cd /home/node/.n8n/custom
+
+# Loop through each directory in /workspace/custom-nodes and create a link to the n8n instance
+for repo_path in /workspace/custom-nodes/*;
+do
+    if [ -d "$repo_path" ]; then
+        echo "Exploring $repo_path folder"
+        repo_name=$(jq -r '.name' "$repo_path/package.json")
+        echo "Checking $repo_name in package.json from $repo_path"
+        if ! grep -q "\"$repo_name\"" /home/node/.n8n/custom/package.json; then
+            echo "Linking $repo_name to n8n"
+            pnpm link "$repo_path"
+        else
+            echo "$repo_name is already linked in package.json"
+        fi
+    fi
+done
